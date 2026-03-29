8df5c8504490b85e33b05f7e2b241923b4db72fc
Martin <martin.jokl123@gmail.com>
2026-03-29 11:49:33 +0200

    Make changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <header>Header</header>
+        <Header />
         {children}
-        <footer>Footer</footer>
       </body>
     </html>
   );
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,20 @@
 
-export default function Home() {
+export default async function Posts() {
+  const response = await fetch("https://dummyjson.com/posts");
+
+  const data: { id: number; title: string }[] = (await response.json()).posts;
+
+  // await new Promise(resolve => setTimeout(resolve, 1000));
+
   return (
     <div className="m-5">
       <h1 className="text-3xl font-bold mb-4">Post</h1>
       <p className="text-zinc-400">yes</p>
+      {data.map((post: { id: number; title: string }) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
