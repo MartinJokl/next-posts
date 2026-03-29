3b9966f082aacba74bcc55dee1e3d9af0d3abb4a
Martin <martin.jokl123@gmail.com>
2026-03-29 11:13:01 +0200

    So stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="m-5">
+      <h1 className="text-3xl">Helllo</h1>
+      <p className="text-zinc-400">My website is this</p>
     </div>
   );
 }
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="m-5">
+      <h1 className="text-3xl font-bold mb-4">Post</h1>
+      <p className="text-zinc-400">yes</p>
+    </div>
+  );
+}
